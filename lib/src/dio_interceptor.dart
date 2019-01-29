4479e088808acc3e7109d04705c9803d43dd8030
Herbert Poul <herbert@poul.at>
2019-01-29 12:13:48 +0100

    use a specific error for invalid username/password.

--- lib/src/dio_interceptor.dart
+++ lib/src/dio_interceptor.dart
@@ -7,6 +7,20 @@ import 'package:ntlm/src/messages/messages.dart';
 
 final log = new Logger("ntlm.dio_interceptor");
 
+/// Tried to authenticate, but (probably) got invalid credentials (username or password).
+class InvalidCredentialsException extends DioError {
+  final String message;
+  final DioError source;
+
+  InvalidCredentialsException(this.message, this.source) :
+        super(response: source.response, message: source.message, type: source.type, stackTrace: source.stackTrace,);
+
+  @override
+  String toString() {
+    return 'InvalidCredentialsException{message=${message},source=${source}}';
+  }
+}
+
 class Credentials {
   String domain;
   String workstation;
@@ -50,11 +64,17 @@ void addNtlmInterceptor(Dio dio, Credentials credentials) {
       domain: credentials.domain,
       workstation: credentials.workstation,
     );
-    final res1 = await authDio.get(e.response.request.path,
-        options: e.response.request.merge(
-          validateStatus: (status) => status == HttpStatus.unauthorized || status == HttpStatus.ok,
-            headers: {HttpHeaders.authorizationHeader: msg1}
-              ..addAll(e.response.request.headers)));
+    final res1 = await (
+        authDio.get(e.response.request.path,
+            options: e.response.request.merge(
+                validateStatus: (status) => status == HttpStatus.unauthorized || status == HttpStatus.ok,
+                headers: {HttpHeaders.authorizationHeader: msg1}
+                  ..addAll(e.response.request.headers)))
+            .catchError((error, stackTrace) {
+              log.fine('Error during type1 message.', error, stackTrace);
+              return Future.error(error, stackTrace);
+        })
+    );
     String res2Authenticate =
         res1.headers[HttpHeaders.wwwAuthenticateHeader]?.first;
     if (res2Authenticate == null) {
@@ -72,12 +92,24 @@ void addNtlmInterceptor(Dio dio, Credentials credentials) {
         username: credentials.username,
         password: credentials.password);
 
-    final res2 = await authDio.get(e.response.request.path,
-        options: e.response.request.merge(
-            headers: {HttpHeaders.authorizationHeader: msg3}
-              ..addAll(e.response.request.headers)));
+    final res2 = await (
+        authDio.get(e.response.request.path,
+            options: e.response.request.merge(
+                headers: {HttpHeaders.authorizationHeader: msg3}
+                  ..addAll(e.response.request.headers)))
+        .catchError((error, stackTrace) {
+          if (error is DioError) {
+            log.fine('Error during authentication request. ${error?.response?.headers}', error, stackTrace);
+            if (error.type == DioErrorType.RESPONSE &&
+                error.response?.statusCode == HttpStatus.unauthorized) {
+              return Future<Response<dynamic>>.error(InvalidCredentialsException('invalid authentication.', error));
+            }
+          }
+          return Future<Response<dynamic>>.error(error, stackTrace);
+        })
+    );
 
-    return res2;
+    return res2 as Response<dynamic>;
   };
 
 
