5a04c6e97a8ba5a61964524443d762d5e27882ee
Herbert Poul <herbert@poul.at>
2019-01-30 10:06:56 +0100

    added much more debugging.

--- lib/src/dio_interceptor.dart
+++ lib/src/dio_interceptor.dart
@@ -40,77 +40,100 @@ void addNtlmInterceptor(Dio dio, Credentials credentials) {
     return options;
   };
   dio.interceptor.response.onSuccess = (Response response) {
-    log.fine('Intercepted onSuccess. ${response.statusCode}');
+    log.fine('Intercepted onSuccess. ${response.statusCode}}');
     return response;
   };
   dio.interceptor.response.onError = (DioError e) async {
-    log.finer('Intercepted onError. ${e.response?.statusCode} for request ${e.response?.request?.path}');
-    if (e.response?.statusCode != HttpStatus.unauthorized) {
-      return e;
-    }
-    final authHeader = e.response.headers[HttpHeaders.wwwAuthenticateHeader];
-    if (authHeader == null || authHeader.first != 'NTLM') {
-      log.warning(
-          'Got a HTTP unauthorized response code, but no NTLM authentication header was set.');
-      return e;
-    }
+    try {
+      log.finer('Intercepted onError. ${e.response?.statusCode} for request ${e.response?.request?.path}');
+      if (e.response?.statusCode != HttpStatus.unauthorized) {
+        return e;
+      }
+      log.finer('headers: ${_debugHttpHeaders(e.response.headers)}');
+      final authHeader = e.response.headers[HttpHeaders.wwwAuthenticateHeader];
+      if (authHeader == null || authHeader.first != 'NTLM') {
+        log.warning(
+            'Got a HTTP unauthorized response code, but no NTLM authentication header was set.');
+        return e;
+      }
 
-    log.fine('Trying to authenticate request.');
+      // FIXME: remove username from log.
+      log.fine('Trying to authenticate request (${credentials.domain}/${credentials.username}).');
 
-    Dio authDio = new Dio(dio.options);
-    authDio.cookieJar = dio.cookieJar;
+      Dio authDio = new Dio(dio.options);
+      authDio.cookieJar = dio.cookieJar;
 
-    String msg1 = createType1Message(
-      domain: credentials.domain,
-      workstation: credentials.workstation,
-    );
-    final res1 = await (
-        authDio.get(e.response.request.path,
-            options: e.response.request.merge(
-                validateStatus: (status) => status == HttpStatus.unauthorized || status == HttpStatus.ok,
-                headers: {HttpHeaders.authorizationHeader: msg1}
-                  ..addAll(e.response.request.headers)))
-            .catchError((error, stackTrace) {
-              log.fine('Error during type1 message.', error, stackTrace);
-              return Future.error(error, stackTrace);
-        })
-    );
-    String res2Authenticate =
-        res1.headers[HttpHeaders.wwwAuthenticateHeader]?.first;
-    if (res2Authenticate == null) {
-      log.warning('No Authenticate header found for response from ${e.response.request.path}.', e, e.stackTrace);
-      return e;
-    }
-    if (!res2Authenticate.startsWith("NTLM ")) {
-      return e;
-    }
-    Type2Message msg2 = parseType2Message(res2Authenticate);
-
-    String msg3 = createType3Message(msg2,
+      String msg1 = createType1Message(
         domain: credentials.domain,
         workstation: credentials.workstation,
-        username: credentials.username,
-        password: credentials.password);
+      );
+      final res1 = await (
+          authDio.get(e.response.request.path,
+              options: e.response.request.merge(
+                  validateStatus: (status) => status == HttpStatus.unauthorized || status == HttpStatus.ok,
+                  headers: {HttpHeaders.authorizationHeader: msg1}
+                    ..addAll(e.response.request.headers)))
+              .catchError((error, stackTrace) {
+            log.fine('Error during type1 message.', error, stackTrace);
+            return Future.error(error, stackTrace);
+          })
+      );
+      String res2Authenticate =
+          res1.headers[HttpHeaders.wwwAuthenticateHeader]?.first;
+      log.finer('Received type1 message response ${res2Authenticate}');
+      if (res2Authenticate == null) {
+        log.warning('No Authenticate header found for response from ${e.response.request.path}.', e, e.stackTrace);
+        return e;
+      }
+      if (!res2Authenticate.startsWith("NTLM ")) {
+        log.warning(
+            'Type1 message response does not return NTLM auth header. ${res1.headers[HttpHeaders.wwwAuthenticateHeader]
+                .toList()}');
+        return e;
+      }
+      Type2Message msg2 = parseType2Message(res2Authenticate);
 
-    final res2 = await (
-        authDio.get(e.response.request.path,
-            options: e.response.request.merge(
-                headers: {HttpHeaders.authorizationHeader: msg3}
-                  ..addAll(e.response.request.headers)))
-        .catchError((error, stackTrace) {
-          if (error is DioError) {
-            log.fine('Error during authentication request. ${error?.response?.headers}', error, stackTrace);
-            if (error.type == DioErrorType.RESPONSE &&
-                error.response?.statusCode == HttpStatus.unauthorized) {
-              return Future<Response<dynamic>>.error(InvalidCredentialsException('invalid authentication.', error));
+      String msg3 = createType3Message(msg2,
+          domain: credentials.domain,
+          workstation: credentials.workstation,
+          username: credentials.username,
+          password: credentials.password);
+
+      final res2 = await (
+          authDio.get(e.response.request.path,
+              options: e.response.request.merge(
+                  headers: {HttpHeaders.authorizationHeader: msg3}
+                    ..addAll(e.response.request.headers)))
+              .catchError((error, stackTrace) {
+            if (error is DioError) {
+              log.fine('Error during authentication request. ${error?.response?.headers}', error, stackTrace);
+              if (error.type == DioErrorType.RESPONSE &&
+                  error.response?.statusCode == HttpStatus.unauthorized) {
+                return Future<Response<dynamic>>.error(InvalidCredentialsException('invalid authentication.', error));
+              }
             }
-          }
-          return Future<Response<dynamic>>.error(error, stackTrace);
-        })
-    );
+            return Future<Response<dynamic>>.error(error, stackTrace);
+          })
+      );
+      log.finer('Received type3 message response. ${res2?.statusCode}.');
 
-    return res2 as Response<dynamic>;
+      return res2 as Response<dynamic>;
+    } catch (e, stackTrace) {
+      String msg = 'error:${e?.runtimeType}';
+      if (e is DioError) {
+        msg = 'code: ${e.response?.statusCode} / ${_debugHttpHeaders(e.response?.headers)}';
+      }
+      log.warning('Error while trying to authenticate. ${msg}', e, stackTrace);
+      rethrow;
+    } finally {
+      log.finer('Finished onError handler.');
+    }
   };
 
+}
 
+String _debugHttpHeaders(HttpHeaders headers) {
+  final ret = Map<String, List<String>>();
+  headers.forEach((key, values) { ret[key] = values; });
+  return ret.toString();
 }
